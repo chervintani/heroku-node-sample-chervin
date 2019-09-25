b824ed35a63cb1683d8eff905fb149112b8002a4
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-25 13:55:01 +0800

    fixed error

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # heroku-node-sample-chervin
 A simple node app deployed to heroku
+
+URL : https://heroku-node-sample-chervin.herokuapp.com/
+GIT : https://heroku-node-sample-chervin.herokuapp.com/
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var port = process.env.port||3000
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+var port = process.env.PORT || 3000
+app.get('/', function (req, res) {
+    res.send('<h1>Hello world</h1>');
 });
 
-http.listen(port, function(){
-  console.log('listening on *: '+ port);
+http.listen(port, '0.0.0.0', function () {
+    console.log('listening on *: ' + port);
 });
