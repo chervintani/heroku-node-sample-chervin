34d7dcdeed1d7d1cdf7f9c73210b63a4de15bcea
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-25 14:05:58 +0800

    update nasad

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 //Serve public directory
 var users = [];
 
